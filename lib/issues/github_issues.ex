ddad24f625d995c956e426e6b6011919db04ed42
Weijun Yin <gw9812@gmail.com>
2014-08-13 10:26:03 +0800

    fetch github issue

---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,19 @@
+defmodule Issues.GithubIssues do
+  
+  @user_agent [ {"User-agent", "Elixir weijun@localgravity.com"}]
+  
+  def fetch(user, project) do
+    issue_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
+
+  def issue_url(user, project) do
+    "https://api.github.com/repos/#{user}/#{project}/issues"
+  end
+
+  def handle_response( %{status_code: 200, body: body} ), do: { :ok, body }
+
+  def handle_response( %{status_code: _, body: body} ), do: { :error, body }  
+  
+end
