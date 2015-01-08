849381cf72ceaf095c8b2001d6040097a0bc6911
Weijun Yin <gw9812@gmail.com>
2015-01-08 17:29:53 +0800

    allow to run on any unix-based platform with Erlang's escript utility

--- mix.exs
+++ mix.exs
@@ -5,6 +5,7 @@ defmodule Issues.Mixfile do
     [app: :issues,
      version: "0.0.1",
      elixir: "~> 1.0.2",
+     escript: escript_config, 
      deps: deps]
   end
 
@@ -29,5 +30,9 @@ defmodule Issues.Mixfile do
       { :httpoison, "~> 0.5" },
       { :jsx, "~> 2.4" }
     ]
+  end
+
+  defp escript_config do
+    [ main_module: Issues.CLI ]
   end
 end
